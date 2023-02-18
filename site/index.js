4fe28c9ba2f45830e4048c4edd8bccc3225598d9
BrotherDonkey <willbjorn@gmail.com>
2023-02-18 12:00:32 -0800

    some improvement to mobile modal

--- site/index.js
+++ site/index.js
@@ -17,3 +17,80 @@ const burger = document.querySelector(".burger");
 burger.onclick = () => {
   burger.classList.toggle("burger-expanded");
 };
+
+class ModalTriggerElement extends HTMLElement {
+  get modal() {
+    if (!this.sharedId) {
+      return null;
+    }
+    return document.querySelector(`[controlled-by="${this.sharedId}"`);
+  }
+  sharedId = this.getAttribute("for");
+
+  connectedCallback() {
+    this.addEventListener("click", () => {
+      this.modal.hidden = !this.modal.hidden;
+    });
+  }
+}
+
+class ModalElement extends HTMLElement {
+  static get observedAttributes() {
+    return ["hidden"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "hidden") {
+      console.log("Custom square element attributes changed.");
+    }
+  }
+
+  constructor() {
+    super();
+  }
+  get controlledBy() {
+    if (!this.sharedId) {
+      return null;
+    }
+    return document.querySelector(`[for="${this.sharedId}"`);
+  }
+  sharedId = this.getAttribute("controlled-by");
+
+  connectedCallback() {
+    window.addEventListener("CloseModals", () => {
+      const burger = document.querySelector(".burger");
+      burger.classList.toggle("burger-expanded");
+
+      this.hidden = true;
+    });
+    // this.addEventListener("click", (e) => {
+    //   console.log({ sharedId: this.sharedId, controlledBy: this.controlledBy });
+    // });
+  }
+}
+
+if (!window.customElements.get("modal-element")) {
+  window.ModalElement = ModalElement;
+  window.customElements.define("modal-element", ModalElement);
+}
+
+if (!window.customElements.get("modal-trigger")) {
+  window.ModalTriggerElement = ModalTriggerElement;
+  window.customElements.define("modal-trigger", ModalTriggerElement);
+}
+
+window.addEventListener("click", (e) => {
+  const content =
+    e.target instanceof Element && e.target.closest(".modal-content");
+  if (content) {
+    return;
+  }
+  const target =
+    e.target instanceof Element && e.target.closest("modal-element");
+
+  if (!target) {
+    return;
+  }
+  console.log("CloseModals");
+  window.dispatchEvent(new CustomEvent("CloseModals"));
+});
